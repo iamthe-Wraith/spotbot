d7f930d1ef416e4ee3e6bbe921913923310cd5d7
Jake Lundberg <allhailthewraith@protonmail.com>
2025-07-09 07:31:47 -0400

    issue/19 :ghost: adds Sentry monitoring

---
+++ src/hooks.server.ts
@@ -0,0 +1,19 @@
+import { PUBLIC_SENTRY_DSN, PUBLIC_SENTRY_TRACES_SAMPLE_RATE } from '$env/static/public';
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: PUBLIC_SENTRY_DSN,
+
+	tracesSampleRate: parseFloat(PUBLIC_SENTRY_TRACES_SAMPLE_RATE) ?? 1.0,
+
+	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: import.meta.env.DEV,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'wraith',
+				project: 'tydalwave'
+			}
+		}),
+		sveltekit()
+	],
 	test: {
 		projects: [
 			{
