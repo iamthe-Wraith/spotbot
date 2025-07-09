d2f32b1a9b1add5220007bc0ac90d955f0b84857
iamthe-Wraith <allhailthewraith@protonmail.com>
2025-07-09 06:56:33 -0400

    feat: :ghost: adds Sentry

---
+++ src/hooks.client.ts
@@ -0,0 +1,22 @@
+import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: 'https://4c5730f405ec43bd9196e8423d320386@o4505018196295680.ingest.us.sentry.io/4509638214680576',
+
+	tracesSampleRate: 1.0,
+
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
+
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
+
+	// If you don't want to use Session Replay, just remove the line below:
+	integrations: [replayIntegration()]
+});
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
---
+++ src/hooks.server.ts
@@ -0,0 +1,18 @@
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: 'https://4c5730f405ec43bd9196e8423d320386@o4505018196295680.ingest.us.sentry.io/4509638214680576',
+
+	tracesSampleRate: 1.0
+
+	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: import.meta.env.DEV,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
