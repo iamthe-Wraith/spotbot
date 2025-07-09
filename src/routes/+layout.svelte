d1e442aa5c68d8be6e122cd247a6c1737a003887
Jake Lundberg <allhailthewraith@protonmail.com>
2025-07-08 21:33:32 -0400

    issue/15 :ghost: adds GitHub link in footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,8 +22,21 @@ import Logo from '$lib/components/Logo.svelte';
 </main>
 
 <footer>
-    <p>TydalWave</p>
-    <p>Copyright {new Date().getFullYear()}</p>
+    <div class="footer-content">
+        <p>TydalWave</p>
+        <p>Copyright {new Date().getFullYear()}</p>
+    </div>
+
+    <nav class="footer-links">
+        <a
+            href="https://github.com/iamthe-Wraith/tydalwave"
+            target="_blank"
+            class="footer-link"
+        >
+            <i class="fa-brands fa-square-github"></i>
+            GitHub
+        </a>
+    </nav>
 </footer>
 
 <Toast />
@@ -54,12 +67,38 @@ import Logo from '$lib/components/Logo.svelte';
     }
 
     footer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
         height: var(--footer-height);
         padding: 0.5rem 1rem;
         border-top: 1px solid var(--primary-200);
 
         p {
             margin: 0;
+        }
+
+        .footer-links {
+            display: flex;
+            align-items: center;
+            gap: 1rem;
+        }
+
+        .footer-link {  
+            display: flex;
+            align-items: center;
+            gap: 0.5rem;
+            text-decoration: none;
+            color: var(--neutral-900);
+
+            &:hover {
+                color: var(--primary-700);
+                cursor: pointer;
+            }
+        }
+
+        .footer-link i {
+            font-size: 1.3rem;
         }
     }
 </style>
